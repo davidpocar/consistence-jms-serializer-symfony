902c89d349c04fc24a6112049042195b214ff78f
David Pocar <pocar.david@gmail.com>
2025-08-20 15:13:31 +0200

    remove matthiasnoback dependency for increased Symfony compatibility

--- tests/DependencyInjection/ConsistenceJmsSerializerExtensionTest.php
+++ tests/DependencyInjection/ConsistenceJmsSerializerExtensionTest.php
@@ -5,34 +5,121 @@ declare(strict_types = 1);
 namespace Consistence\JmsSerializer\SymfonyBundle\DependencyInjection;
 
 use Consistence\JmsSerializer\Enum\EnumSerializerHandler;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
-class ConsistenceJmsSerializerExtensionTest extends \Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase
+class ConsistenceJmsSerializerExtensionTest extends \PHPUnit\Framework\TestCase
 {
-
-	/**
-	 * @return \Symfony\Component\DependencyInjection\Extension\ExtensionInterface[]
-	 */
-	protected function getContainerExtensions(): array
-	{
-		return [
-			new ConsistenceJmsSerializerExtension(),
-		];
-	}
 
 	public function testRegisterSerializerHandler(): void
 	{
-		$this->load();
+		$container = self::createContainer();
+		$container->registerExtension(new ConsistenceJmsSerializerExtension());
 
-		$this->assertContainerBuilderHasService(
-			'consistence.jms_serializer.enum.enum_serializer_handler',
-			EnumSerializerHandler::class
-		);
-		$this->assertContainerBuilderHasServiceDefinitionWithTag(
-			'consistence.jms_serializer.enum.enum_serializer_handler',
-			'jms_serializer.subscribing_handler'
+		self::loadRegisteredExtension($container);
+
+		$serviceId = 'consistence.jms_serializer.enum.enum_serializer_handler';
+		self::assertContainerHasService($container, $serviceId);
+		self::assertContainerServiceIsOfType($container, $serviceId, EnumSerializerHandler::class);
+		self::assertContainerServiceHasTag(
+			$container,
+			$serviceId,
+			'jms_serializer.subscribing_handler',
+			[]
 		);
 
-		$this->compile();
+		$container->compile();
+	}
+
+	private static function createContainer(): ContainerBuilder
+	{
+		$container = new ContainerBuilder(new ParameterBag([]));
+		$container->getCompilerPassConfig()->setOptimizationPasses([]);
+		$container->getCompilerPassConfig()->setRemovingPasses([]);
+		$container->getCompilerPassConfig()->setAfterRemovingPasses([]);
+
+		return $container;
+	}
+
+	public static function assertContainerHasService(
+		ContainerBuilder $container,
+		string $serviceId
+	): void
+	{
+		Assert::assertTrue(
+			$container->has($serviceId),
+			sprintf('Expecting the container to have service `%s`.', $serviceId)
+		);
+	}
+
+	public static function assertContainerServiceIsOfType(
+		ContainerBuilder $container,
+		string $serviceId,
+		string $expectedClassString
+	): void
+	{
+		$serviceDefinition = $container->findDefinition($serviceId);
+
+		Assert::assertSame(
+			$expectedClassString,
+			$container->getParameterBag()->resolveValue($serviceDefinition->getClass()),
+			sprintf('Expecting the service `%s` to be of type `%s`.', $serviceId, $expectedClassString)
+		);
+	}
+
+	private static function assertContainerServiceHasTag(
+		ContainerBuilder $container,
+		string $serviceId,
+		string $tagName
+	): void
+	{
+		$serviceDefinition = $container->findDefinition($serviceId);
+
+		Assert::assertTrue(
+			$serviceDefinition->hasTag($tagName),
+			sprintf('Expecting the service `%s` to have tag `%s`.', $serviceId, $tagName)
+		);
+	}
+
+	/**
+	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 * @param mixed[][]|array $configuration
+	 */
+	public static function loadRegisteredExtension(
+		ContainerBuilder $container,
+		array $configuration = []
+	): void
+	{
+		$registeredExtensionsCount = count($container->getExtensions());
+
+		assert(
+			$registeredExtensionsCount === 1,
+			sprintf('There are %d extensions registered but one is expected', $registeredExtensionsCount)
+		);
+
+		self::loadRegisteredExtensionsUsingCommonConfiguration($container, $configuration);
+	}
+
+	/**
+	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 * @param mixed[][]|array $configuration
+	 */
+	private static function loadRegisteredExtensionsUsingCommonConfiguration(
+		ContainerBuilder $container,
+		array $configuration = []
+	): void
+	{
+		foreach ($container->getExtensions() as $extension) {
+			if ($extension instanceof PrependExtensionInterface) {
+				$extension->prepend($container);
+			}
+		}
+
+		foreach ($container->getExtensions() as $extension) {
+			$extension->load([$configuration], $container);
+		}
 	}
 
 }
